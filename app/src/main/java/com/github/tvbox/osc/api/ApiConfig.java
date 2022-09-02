e523cdce974a92545503fed493852883284f5f41
ichenczh <ichenczh@gmail.com>
2022-09-02 22:33:13 +0800

    22.33

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://刚刚.live/%E7%8C%AB");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
